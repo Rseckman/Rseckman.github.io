0c4df341e6bca9fd7f630b9af77ab532ca17d1b1
Ryan <69523694+Rseckman@users.noreply.github.com>
2021-03-23 11:29:08 -0400

    changed resume link to google drive resume

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -18,7 +18,7 @@ const Contact = () => (
             <ListGroup.Item action variant="info" target="_blank" href="https://www.linkedin.com/in/ryan-seckman">
               <strong>LinkedIn</strong>
             </ListGroup.Item>
-            <ListGroup.Item action variant="info" target="_blank" href="https://docs.google.com/document/d/1bKiF9OigWpjZqPHbOt2uvfqWt7EKNmrRecNH-uR5hV4/edit?usp=sharing">
+            <ListGroup.Item action variant="info" target="_blank" href="https://drive.google.com/file/d/1nd4nxEWWzxPlB6EicSWqGq1K4aTnIkIA/view?usp=sharing">
               <strong>My Resume</strong>
             </ListGroup.Item>
         </ListGroup>
